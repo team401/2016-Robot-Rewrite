6b1e5947b35e4a5920634e28eb19807fe25ec773
Zachary Kozar <zachkozar@masterzach32.net>
2016-11-29 14:18:02 -0500

    Renamed project and added strongback to the repo

---
+++ GradleRIO/src/main/java/org/team401/robot/Robot.java
@@ -0,0 +1,46 @@
+package org.team401.robot2016;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+import org.strongback.Strongback;
+import org.strongback.components.Motor;
+import org.strongback.hardware.Hardware;
+
+public class Robot extends IterativeRobot {
+
+    @Override
+    public void robotInit() {
+        Strongback.
+        Motor leftFrontMotor = Hardware.Motors.talonSRX
+    }
+
+    @Override
+    public void disabledInit() {
+
+    }
+
+    @Override
+    public void autonomousInit() {
+
+    }
+
+    @Override
+    public void teleopInit() {
+
+    }
+
+    @Override
+    public void disabledPeriodic() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+}
--- GradleRIO/src/main/java/org/usfirst/frc/team0401/Robot.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc.team0401;
-
-import edu.wpi.first.wpilibj.IterativeRobot;
-
-public class Robot extends IterativeRobot {
-
-    // robot code
-}
