6ac4cd017ee7378e7595b8b5e14f8ce0dab91613
Zach Kozar <zachkozar@masterzach32.net>
2016-11-29 17:17:33 -0500

    Obviously need to fix port values, but robot should be drivable?

--- CompetitionRobot/src/main/java/org/team401/robot/Robot.java
+++ CompetitionRobot/src/main/java/org/team401/robot/Robot.java
@@ -1,7 +1,9 @@
 package org.team401.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+
 import org.strongback.components.Motor;
+import org.strongback.components.ui.FlightStick;
 import org.strongback.drive.TankDrive;
 import org.strongback.hardware.Hardware;
 
@@ -9,8 +11,11 @@ public class Robot extends IterativeRobot {
 
     private TankDrive drive;
 
+    private FlightStick leftDriveController, rightDriveController, armController;
+
     @Override
     public void robotInit() {
+        // TODO fix motor ports
         Motor leftGearbox = Motor.compose(
                 Hardware.Motors.talonSRX(0),
                 Hardware.Motors.talonSRX(1),
@@ -19,11 +24,17 @@ public class Robot extends IterativeRobot {
                 Hardware.Motors.talonSRX(3),
                 Hardware.Motors.talonSRX(4),
                 Hardware.Motors.talonSRX(5)).invert();
+        // TODO add hi/lo gear solenoids
         drive = new TankDrive(leftGearbox, rightGearbox);
+
+        // TODO fix joystick ports
+        leftDriveController = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
+        rightDriveController = Hardware.HumanInterfaceDevices.logitechAttack3D(1);
+        armController = Hardware.HumanInterfaceDevices.logitechAttack3D(2);
     }
 
     @Override
-    public void disabledInit() {
+    public void teleopInit() {
 
     }
 
@@ -33,22 +44,13 @@ public class Robot extends IterativeRobot {
     }
 
     @Override
-    public void teleopInit() {
-
-    }
-
-    @Override
-    public void disabledPeriodic() {
-
+    public void teleopPeriodic() {
+        // read values from joystick and drive (maybe)
+        drive.tank(leftDriveController.getPitch().read(), rightDriveController.getPitch().read());
     }
 
     @Override
     public void autonomousPeriodic() {
-
-    }
-
-    @Override
-    public void teleopPeriodic() {
 
     }
 }
