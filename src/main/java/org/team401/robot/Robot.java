06be0ff274e891b2e044d79524a04ea997b279a4
Briangulator <brian174174@gmail.com>
2017-01-28 16:05:10 -0500

    Code now should work with gutted Quetz
    Except for port numbers, those are default.

--- src/main/java/org/team401/robot/Robot.java
+++ src/main/java/org/team401/robot/Robot.java
@@ -23,21 +23,20 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.strongback.Strongback;
 import org.strongback.SwitchReactor;
+import org.strongback.components.Motor;
 import org.strongback.components.Solenoid;
 import org.strongback.components.Switch;
 import org.strongback.components.ui.FlightStick;
+import org.strongback.drive.TankDrive;
 import org.strongback.hardware.Hardware;
 
 import org.team401.robot.arm.Arm;
 import org.team401.robot.arm.CannonShooter;
-import org.team401.robot.arm.commands.PushBoulder;
-import org.team401.robot.chassis.QuezDrive;
 import org.team401.robot.components.DartLinearActuator;
-import org.team401.robot.path.FalconPathPlanner;
 
 public class Robot extends IterativeRobot {
 
-    private QuezDrive chassis;
+    private TankDrive drive;
     private Arm arm;
 
     private FlightStick leftDriveController, rightDriveController, armController;
@@ -47,66 +46,22 @@ public class Robot extends IterativeRobot {
         Strongback.configure()
                 .recordDataToFile("/home/lvuser/")
                 .recordEventsToFile("/home/lvuser/", 2097152);
-
-        Solenoid shifter = Hardware.Solenoids.doubleSolenoid(4, 1, Solenoid.Direction.RETRACTING);
-        chassis = new QuezDrive(shifter, false);
+        drive = new TankDrive(
+                Motor.compose(
+                        Hardware.Motors.talon(0),
+                        Hardware.Motors.talon(1)),
+                Motor.compose(
+                        Hardware.Motors.talon(2),
+                        Hardware.Motors.talon(3)));
 
         Solenoid shooter = Hardware.Solenoids.doubleSolenoid(5, 2, Solenoid.Direction.RETRACTING);
+
         arm = new Arm(new DartLinearActuator(),
                 new CannonShooter(shooter, false, false));
 
         leftDriveController = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
         rightDriveController = Hardware.HumanInterfaceDevices.logitechAttack3D(1);
         armController = Hardware.HumanInterfaceDevices.logitechAttack3D(2);
-
-        SmartDashboard.putBoolean("Demo Mode", false);
-        SmartDashboard.putBoolean("Auto Shooting Mode", true);
-
-        Switch gearToggle = rightDriveController.getButton(2);
-        //Switch demoMode = armController.getButton(9); // change these buttons
-        //Switch toggleShootMode = armController.getButton(10);
-        Switch trigger = armController.getTrigger();
-        Switch spinOut = armController.getButton(5);
-        //Switch stopWheels = () -> armController.getDPad(0).getDirection() == -1;
-        Switch spinIn = armController.getButton(3); // ??????????
-
-        BetterSwitch oneButtonShoot = new BetterSwitch(
-                () -> SmartDashboard.getBoolean("Auto Shooting Mode", false));
-
-        SwitchReactor switchReactor = Strongback.switchReactor();
-
-        /*switchReactor.onTriggered(gearToggle,
-                () -> chassis.toggleGear());
-        /*switchReactor.onTriggered(demoMode,
-                () -> Strongback.submit(new ToggleDemoMode(chassis, arm)));
-        switchReactor.onTriggered(toggleShootMode,
-                () -> SmartDashboard.putBoolean("Auto Shooting Mode", !SmartDashboard.getBoolean("Auto Shooting Mode")));*/
-
-        /*switchReactor.onTriggeredSubmit(Switch.and(oneButtonShoot, trigger),
-                () -> new FireBoulder(arm, armController.getThrottle().read()));*/
-
-        /*switchReactor.onTriggered(Switch.and(oneButtonShoot.invert(), spinOut),
-                () -> arm.getShooter().spinOut(armController.getThrottle().read()));
-        switchReactor.onTriggered(Switch.and(oneButtonShoot.invert(), spinIn),
-                () -> arm.getShooter().spinIn());
-        switchReactor.onTriggered(Switch.and(oneButtonShoot.invert(), stopWheels),
-                () -> arm.getShooter().stop());*/
-
-        switchReactor.onTriggeredSubmit(trigger,
-                () -> new PushBoulder(arm.getShooter().getSolenoid()));
-        switchReactor.onTriggered(spinIn,
-                () -> arm.getShooter().spinIn());
-        switchReactor.whileTriggered(spinOut,
-                () -> arm.getShooter().spinOut(armController.getThrottle().read()*-1));
-        switchReactor.onUntriggered(Switch.or(spinIn, spinOut),
-                () -> arm.getShooter().stop());
-
-
-        Strongback.dataRecorder()
-                .register("Gear", chassis.highGear())
-                //.register("Arm Unlock", armController.getThumb())
-                .register("Top", arm.getDart().getTopHallEffect())
-                .register("Bottom", arm.getDart().getBottomHallEffect());
     }
 
     @Override
@@ -116,27 +71,38 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
-        chassis.drive(leftDriveController.getPitch().read(), rightDriveController.getPitch().read());
-        if (armController.getButton(2).isTriggered())
+        drive.tank(leftDriveController.getPitch().read(), rightDriveController.getPitch().read());
+        if (armController.getThumb().isTriggered())
             arm.getDart().drive(armController.getPitch().read());
         else
             arm.getDart().stop();
+        if(armController.getTrigger().isTriggered())
+            arm.getShooter().getSolenoid().extend();
+        else
+            arm.getShooter().getSolenoid().retract();
+        if(armController.getButton(3).isTriggered())
+            arm.getShooter().spinOut(armController.getThrottle().read());
+        else if(armController.getButton(4).isTriggered())
+            arm.getShooter().spinIn();
+        else
+            arm.getShooter().stop();
     }
 
     @Override
     public void autonomousInit() {
         Strongback.start();
-        FalconPathPlanner p = new FalconPathPlanner(new double[][] {});
-        p.calculate(10, .02, 2);
     }
 
     @Override
     public void autonomousPeriodic() {
-
     }
 
     @Override
     public void disabledInit() {
         Strongback.disable();
+    }
+
+    public void disabledPeriodic(){
+        drive.tank(0,0);
     }
 }
